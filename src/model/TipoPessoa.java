aef66b6128be5180221a3091967a89d7beb74ac2
curso <curso@g1c20.windows.cin.ufpe.br>
2016-09-22 08:27:04 -0300

    Criação das classes de modelo (Aluno, CpfValidator, Pessoa, TipoPessoa, TipoSexo, Pessoa), e alteração do nome do pacote para model.

---
+++ src/model/TipoPessoa.java
@@ -0,0 +1,31 @@
+package model;
+
+public enum TipoPessoa {
+
+	DISCENTE("Aluno", 6),
+	DOCENTE("Professor", 7);
+
+	private String nomeApresentacao;
+	private int tempo;
+
+	private TipoPessoa(String nomeApresentacao, int tempo){
+		this.nomeApresentacao = nomeApresentacao;
+		this.tempo = tempo;
+	}
+
+	public String getNomeApresentacao() {
+		return nomeApresentacao;
+	}
+
+	public void setNomeApresentacao(String nomeApresentacao) {
+		this.nomeApresentacao = nomeApresentacao;
+	}
+
+	public int getTempo() {
+		return tempo;
+	}
+
+	public void setTempo(int tempo) {
+		this.tempo = tempo;
+	}
+}
